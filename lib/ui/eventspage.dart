26386e383d89ffa44288093e75dc0089a3343bd9
admin <tobynwude@gmail.com>
2022-03-01 20:11:15 +0100

    Added listBuilder for the eventsPage

--- lib/ui/eventspage.dart
+++ lib/ui/eventspage.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:viicsoft_inventory_app/models/events.dart';
 
 import '../services/apis/event_api.dart';
 
 class EventsPage extends StatefulWidget {
-  const EventsPage({Key? key}) : super(key: key);
+  const EventsPage({Key? key, required this.events}) : super(key: key);
+  final List<Event> events;
 
   @override
   _EventsPageState createState() => _EventsPageState();
 }
 
 class _EventsPageState extends State<EventsPage> {
+  late final List<Event> events;
   late Future<List<Event>> futureEvent;
   final EventAPI _eventApi = EventAPI();
 
@@ -22,39 +25,27 @@ class _EventsPageState extends State<EventsPage> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Fetch Data Example',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Fetch Data Example'),
-        ),
-        body: Center(
-          child: FutureBuilder<List<Event>>(
-            future: futureEvent,
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                      itemCount: snapshot.data!.length,
-                      itemBuilder: (_, index) => Container(
-                          height: 75,
-                          color: Colors.white,
-                          child: Center(child: Text("${snapshot.data!}"),
-                          ),
-                      )
-                  );
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
-
-              // By default, show a loading spinner.
-              return const CircularProgressIndicator();
-            },
-          ),
-        ),
-      ),
-    );
+    return
+      ListView.builder(
+          itemCount: events == null ? 0 : events.length,
+          itemBuilder: (BuildContext context, int index) {
+            return
+              Card(
+                  child: InkWell(
+                    onTap: () {
+                      // Navigator.push(
+                      //   context,
+                      //   MaterialPageRoute(
+                      //       builder: (context) => DetailWidget(cases[index])),
+                      // );
+                    },
+                    child: ListTile(
+                      leading: Icon(Icons.person),
+                      title: Text(events[index].eventName.toString()),
+                      subtitle: Text(events[index].eventLocation.toString()),
+                    ),
+                  )
+              );
+          });
   }
 }
