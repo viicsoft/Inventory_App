6667a1e3753b1fc747e87c3f6cc2eef01b10ab3a
admin <tobynwude@gmail.com>
2022-03-10 10:09:00 +0100

    added addCategory api

--- lib/services/apis/category_api.dart
+++ lib/services/apis/category_api.dart
@@ -1,11 +1,8 @@
 import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:viicsoft_inventory_app/models/category.dart';
 import 'package:viicsoft_inventory_app/services/api.dart';
 import 'package:http/http.dart' as http;
-import 'package:viicsoft_inventory_app/ui/Menu/add_category.dart';
 import '../sharedpref.dart';
 
 class CategoryAPI extends BaseAPI {
@@ -31,23 +28,50 @@ class CategoryAPI extends BaseAPI {
   }
 
 
+   Future<http.StreamedResponse> addCategory (String name, XFile image) async{
+     final String token = await SharedPrefrence().getToken();
+     Map<String, String> headers = {
+       "X-Api-Key": "632F2EC9771B6C4C0BDF30BE21D9009B",
+       'x-token': token,
+     };
+    //create multipart request for POST or PATCH method
+    var request = http.MultipartRequest("POST",
+        Uri.parse(super.addEquipmentsCategoryPath));
+    //add header in http request
+     request.headers.addAll(headers);
 
-  Future<http.Response> addCategory(String categoryName, XFile? categoryImage) async {
-    final String token = await SharedPrefrence().getToken();
-    var body = jsonEncode({
-        'name': categoryName,
-        'image': categoryImage,});
-    http.Response response =
-    await http.post(Uri.parse(super.addEquipmentsCategoryPath), headers: {
-          "X-Api-Key": "632F2EC9771B6C4C0BDF30BE21D9009B",
-          "Content-Type": "application/json",
-          'Accept': 'application/json',
-          'x-token': token,
-        }, body: body);
-    if (response.statusCode == 200) {
-      return response;
-    } else {
-      throw Exception('Failed to load Equipments');
-    }
+    //add text fields
+    request.fields["name"] = name;
+    //create multipart using filepath, string or bytes
+    var pic = await http.MultipartFile.fromPath("image", image.path);
+    //add multipart to request
+    request.files.add(pic);
+    var response = await request.send();
+
+    //Get the response from the server
+    var responseData = await response.stream.toBytes();
+    var responseString = String.fromCharCodes(responseData);
+    print(responseString);
+    print(response.statusCode);
+    return response;
   }
+
+  // Future<http.Response> addCategory(String categoryName, XFile? categoryImage) async {
+  //   final String token = await SharedPrefrence().getToken();
+  //   var body = jsonEncode({
+  //       'name': categoryName,
+  //       'image': categoryImage,});
+  //   http.Response response =
+  //   await http.post(Uri.parse(super.addEquipmentsCategoryPath), headers: {
+  //         "X-Api-Key": "632F2EC9771B6C4C0BDF30BE21D9009B",
+  //         "Content-Type": "application/json",
+  //         'Accept': 'application/json',
+  //         'x-token': token,
+  //       }, body: body);
+  //   if (response.statusCode == 200) {
+  //     return response;
+  //   } else {
+  //     throw Exception('Failed to load Equipments');
+  //   }
+  // }
 }
